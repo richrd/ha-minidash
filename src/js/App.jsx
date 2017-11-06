319a553460427b133032c9279d4d5015fc8c7103
Richard Lewis <richrd.lewis@gmail.com>
2017-11-06 18:35:45 +0200

    Implemented opening and closing nav with horizontal swipes.

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -4,8 +4,36 @@ import PropTypes from 'prop-types';
 
 import { stateToProps } from './utilities';
 import { tryConnect } from './state/actions/connection';
+import { openNavigation, closeNavigation } from './utilities/ui';
 
 import HeaderBar from './HeaderBar';
+import SwipeContainer from './partials/SwipeContainer';
+
+
+function onSwipe(angle, distance, start, end) {
+  // Minimum swipe distance
+  const minDistance = 50;
+  // Maximum angle compared to x axis
+  const maxAngle = 25;
+
+  if (distance < minDistance) {
+    return;
+  }
+
+  let action = openNavigation;
+  if (start.x > end.x) {
+    action = closeNavigation;
+  }
+
+  // Normalize the angle
+  if (Math.abs(angle) > 90) {
+    angle = 180 - Math.abs(angle);
+  }
+
+  if (Math.abs(angle) <= maxAngle) {
+    action();
+  }
+}
 
 class App extends Component {
   static propTypes = {
@@ -31,12 +59,14 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <SwipeContainer
+        onSwipe={onSwipe}
+      >
         <HeaderBar />
         <main id="component-container">
           {this.props.children}
         </main>
-      </div>
+      </SwipeContainer>
     );
   }
 }
