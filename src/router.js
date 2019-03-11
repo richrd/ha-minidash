22a29d5c39ffa10be69a1fd752ac149637fe66e2
Richard Lewis <richrd.lewis@gmail.com>
2019-03-12 01:20:39 +0200

    Cleanup router.

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
+    // TODO: implement default route instead of empty view
     // {
     //   path: "/",
     //   name: "home",
@@ -30,13 +31,5 @@ export default new Router({
       path: "/group/:id",
       component: Group,
     },
-    // {
-    //   path: '/info',
-    //   name: 'info',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
   ],
 });
